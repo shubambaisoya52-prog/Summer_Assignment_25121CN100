f31663cb0a93dfacc8acd1487b609d1d4e953d99
shubambaisoya52-prog <shubambaisoya52@gmail.com>
2026-06-21 23:11:53 +0530

    Add files via upload

---
+++ day5(1).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for(int i = 1; i < num; i++) {
+        if(num % i == 0)
+            sum += i;
+    }
+
+    if(sum == num)
+        printf("%d is a Perfect Number", num);
+    else
+        printf("%d is not a Perfect Number", num);
+
+    return 0;
+}
