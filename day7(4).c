2f2ab098e0015d509c242872abb077364aed9bbd
shubambaisoya52-prog <shubambaisoya52@gmail.com>
2026-06-30 22:41:56 +0530

    Add files via upload

---
+++ day7(4).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void reverse(int n)
+{
+    if (n == 0)
+        return;
+
+    printf("%d", n % 10);
+    reverse(n / 10);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Reverse = ");
+    reverse(n);
+
+    return 0;
+}
