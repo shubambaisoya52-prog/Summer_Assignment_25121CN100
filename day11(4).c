a9143027cb6c4331edda9ad46e5f0ce3774ff79b
shubambaisoya52-prog <shubambaisoya52@gmail.com>
2026-06-30 22:53:15 +0530

    Add files via upload

---
+++ day11(4).c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+long long factorial(int n)
+{
+    int i;
+    long long fact = 1;
+
+    for(i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    return fact;
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Factorial = %lld", factorial(n));
+
+    return 0;
+}
